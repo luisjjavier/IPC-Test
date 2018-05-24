09f5cc05c950477bb1e4156a58e53278641b5884
LuisJavier <javier.luis29@gmail.com>
2018-05-24 14:51:13 -0400

    Add project files.

---
+++ InterProd.ServerApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterProd.ServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
